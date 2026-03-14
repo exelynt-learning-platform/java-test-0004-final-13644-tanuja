6fedfce4329c075c4f92184c30a8bf64c28ba1e9
Tanuja Kohale <kohaletanuja40@gmail.com>
2026-03-14 16:52:18 +0530

    Update ConcentricSquare.java

--- ConcentricSquare.java
+++ ConcentricSquare.java
@@ -1,10 +1,10 @@
 public class ConcentricSquare {
 
-    private static final int CENTER_VALUE = 4;
+    private static final int MAX_VALUE = 4;
 
     public static void main(String[] args) {
 
-        int size = 2 * CENTER_VALUE - 1;
+        int size = 2 * MAX_VALUE - 1;
 
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
@@ -15,7 +15,7 @@ public class ConcentricSquare {
                 int right = size - 1 - col;
 
                 int minDistance = Math.min(Math.min(top, bottom), Math.min(left, right));
-                int value = CENTER_VALUE - minDistance;
+                int value = MAX_VALUE - minDistance;
 
                 System.out.print(value + " ");
             }
